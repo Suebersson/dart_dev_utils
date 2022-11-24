aa3bfb9a91a1d32a7d1ac8e8b888a21f6abb6970
Suebersson <suebersson.dev@gmail.com>
2022-11-24 19:56:32 -0300

    dart_dev_utils

--- gitUpgrade.bat
+++ gitUpgrade.bat
@@ -11,7 +11,7 @@
 :: Já na arquitetura de projetos dart e flutter basta apenas criar o arquivo .pubignore, 
 :: caso ele não exista, e adicionar *.bat
 ::
-@ECHO off
+@echo off
 :: ########## Caminhos de diretorios ###########
 ::@echo %cd%
 ::@echo %~dp0
@@ -29,6 +29,7 @@ for %%f in ("%cd%") do set thisFolder=%%~nxf
 :: Definir o nome da conta de usuario na variavel 'GIT_USER' manualmente
 ::set GIT_USER=myUserName
 :: set GIT_USER ::: Ler a conta de usuario logando no git usando 'git config --get' ou '--global'
+::CALL cmd /K "git config --get user.name"
 for /f %%f in ('git config --get user.name') do set GIT_USER=%%f
 
 @echo.
@@ -40,12 +41,19 @@ for /f %%f in ('git config --get user.name') do set GIT_USER=%%f
 @echo ================================================================================================
 @echo.
 
-::git init
+:::::::::::::::::::::::::::: upgrade ::::::::::::::::::::::::::::::
 git add .
 git commit -m %thisFolder%
-::git remote add %BRANCH% %GIT_DOMAIN%%GIT_USER%/%thisFolder%.git
 git pull %BRANCH% main
 git push -u %BRANCH% main
+
+:::::::::::::::::::::::::::: first commit ::::::::::::::::::::::::::::::
+@REM git init
+@REM git add .
+@REM git commit -m %thisFolder%
+@REM git branch -M main
+@REM git remote add %BRANCH% %GIT_DOMAIN%%GIT_USER%/%thisFolder%.git
+@REM git push -u %BRANCH% main
 
 @echo.
 @echo.
